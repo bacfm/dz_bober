3d2726eeac292f395eb715fd3c2e78f3767f1e26
bacfm <rv.bobrovskiy@gmail.com>
2017-06-01 11:28:49 +0300

    Full dz

--- DZ_JS/JS_18.05.17/main.js
+++ DZ_JS/JS_18.05.17/main.js
@@ -1,5 +1,5 @@
 
-function Bar(name, drinks = [], order = []){
+/*function Bar(name, drinks = [], order = []){
 	this.name = name;
 	this.drinks = drinks;
 	this.order = order;
@@ -34,14 +34,22 @@ Drink.prototype = Object.create(Bar.prototype);
 
 Bar.prototype.tip = 0;
 
-Bar.prototype.addBarmen = function(name){
-	this.bar.push(name);
-	return this;
+Waiter.prototype.getPlace = "Waiter";
+Barman.prototype.getPlace = "Barman";
+
+
+Bar.prototype.addStaff = function(name){
+	if(name.getPlace === "Waiter"){
+		this.waiter.push(name);
+		return this;
+	} else if(name.getPlace === "Barman"){
+		this.bar.push(name);
+		return this;
+	} else {
+		return alert("Такой должности нет!");
+	}
 };
-Bar.prototype.addWaiter = function(name){
-	this.waiter.push(name);
-	return this;
-};
+
 Bar.prototype.addDrinks = function (title, num){
 	var water = new Drink(title, num);
 	this.drinks.push(water);
@@ -65,7 +73,7 @@ Bar.prototype.removeStaff = function (name){
 
  Waiter.prototype.addOrder = function (name, title, num){
  	var order = new Drink(title, num);
- 	 if(bar.drinks.find(product => product.title === title)){
+ 	 if(name.drinks.find(product => product.title === title)){
  	return name.order.push(order);
  } else {
  	return alert("Такого напитка нет.");
@@ -76,9 +84,21 @@ Barman.prototype.theBestDrink = function(name){
 	return console.log(`Сегодня фирменный коктейль - ${name}!`);
 };
 
-// Barman.prototype.doOrder = function (){
+ Barman.prototype.doOrder = function (name, title, quantity){
+ 	for(let i = 0; i<name.order.length; i++){
+ 	if(name.order[i].title === title){
+ 		let drinkQuantity = name.order[i].num -= quantity;
+ 		if(drinkQuantity > 0){
+ 			return name.order[i].num -= quantity;
+ 		} else {
+ 			return alert("У нас столько нет!");
+ 		}
 
-// }
+ 	} else {
+ 		return alert("У нас нету такого напитка");
+ 	}
+}
+ }
 
 
 const bar = new Bar("Alcohol", [], []);
@@ -90,15 +110,14 @@ const bar = new Bar("Alcohol", [], []);
 
  const edic = new Barman("Edic", 26);
  const alik = new Barman("Alik", 32);
-
-
-bar
-	.addWaiter(sam)
-	.addWaiter(bob)
-	.addWaiter(elise)
-	.addWaiter(don)
-	.addBarmen(edic)
-	.addBarmen(alik);
+console.log(bob.getPlace);
+ bar
+	.addStaff(sam)
+	.addStaff(bob)
+	.addStaff(elise)
+	.addStaff(don)
+	.addStaff(edic)
+	.addStaff(alik);
 
 sam.addTips(1000);
 bob.addTips(500);
@@ -112,4 +131,275 @@ bar.addDrinks("medovyxa", 30);
  sam.addOrder(bar, "cola", 5);
  sam.addOrder(bar, "vodka", 5);
 
-edic.theBestDrink("Tequila");
+ edic.theBestDrink("Tequila");
+
+ edic.doOrder(bar, "cola", 6);
+
+console.log(bar);
+*/
+//-----------------------CONSTRUCTOR-----------------------------------
+/*let Drink = {
+	constructor(title, num){
+		this.title = title;
+		this.num = num;
+
+		return this;
+	}
+}
+
+let Bar = {
+	constructor(name, drinks = [], order = [], barman = [], waiter = []){
+		this.name = name;
+		this.drinks = drinks;
+		this.order = order;
+		this.barman = barman;
+		this.waiter = waiter;
+
+		return this;
+	},
+	addStaff(name){
+	if(name.getPlace === "Waiter"){
+		this.waiter.push(name);
+		return this;
+	} else if(name.getPlace === "Barman"){
+		this.barman.push(name);
+		return this;
+	} else {
+		return alert("Такой должности нет!");
+	}
+},
+addDrinks(title, num){
+	var water = Object.create(Drink).constructor(title, num);
+	this.drinks.push(water);
+	return this;
+},
+removeStaff(name){
+	if(this.bar.find(person => person.name === name)){
+		return delete person.name;
+	} else if(this.waiter.find(person => person.name === name)){
+		return delete person.name;
+	} else{ console.log("Такого сотрудника нету!!!!")}
+}
+}
+
+
+
+
+let Person = {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+
+		return this;
+	}
+}
+
+
+let Barman = Object.create(Person);
+
+Barman.constructor = function(name, age, topDrink) {
+	Person.constructor.apply(this, arguments);
+	this.topDrink = topDrink;
+
+	return this;
+}
+Barman.getPlace = "Barman";
+Barman.theBestDrink = function(){
+	 console.log(`Сегодня фирменный коктейль - ${this.topDrink}!`);
+};
+
+ Barman.doOrder = function (name, title, quantity){
+ 	for(let i = 0; i<name.order.length; i++){
+ 	if(name.order[i].title === title){
+ 		let drinkQuantity = name.order[i].num -= quantity;
+ 		if(drinkQuantity > 0){
+ 			return name.order[i].num -= quantity;
+ 		} else {
+ 			return alert("У нас столько нет!");
+ 		}
+
+ 	} else {
+ 		return alert("У нас нету такого напитка");
+ 	}
+}
+ }
+
+let Waiter = Object.create(Person);
+
+Waiter.constructor = function(name, age){
+	Person.constructor.apply(this, arguments);
+
+	return this;
+}
+Waiter.getPlace = "Waiter";
+Waiter.addOrder = function (name, title, num){
+ 	var order = Object.create(Drink).constructor(title, num);
+ 	 if(name.drinks.find(product => product.title === title)){
+ 	return name.order.push(order);
+ } else {
+ 	return alert("Такого напитка нет.");
+ 	}
+ }
+
+
+let bar = Object.create(Bar).constructor("Bar");
+
+
+
+
+
+let bob = Object.create(Waiter).constructor("Bob", 23);
+let lucy = Object.create(Waiter).constructor("Lucy", 19);
+let jack = Object.create(Waiter).constructor("Jack", 24);
+let tony = Object.create(Barman).constructor("Tony", 25, "Tequila");
+let marley = Object.create(Barman).constructor("Marley", 27, "Vodka");
+
+bar
+	.addStaff(bob)
+	.addStaff(lucy)
+	.addStaff(jack)
+	.addStaff(tony)
+	.addStaff(marley)
+	.addDrinks("vodka", 10)
+	.addDrinks("juce", 5)
+	.addDrinks("Byrbone", 3);
+
+	// bob.addOrder(bar, "vodka", 5);
+
+	// marley.doOrder(bar, "vodka", 6);
+// tony.theBestDrink();
+
+
+console.log(bar);
+
+*/
+
+//------------------------CLASS----------------------------------
+/*class Drink{
+	constructor(title, num){
+		this.title = title;
+		this.num = num;
+	}
+}
+
+class Bar{
+	constructor(name, drinks = [], order = []){
+		this.name = name;
+		this.drinks = drinks;
+		this.order = order;
+		this.barman = [];
+		this.waiter = [];
+	}
+	addStaff(name){
+	if(name.place === "Waiter"){
+		this.waiter.push(name);
+		return this;
+	} else if(name.place === "Barman"){
+		this.barman.push(name);
+		return this;
+	} else {
+		return alert("Такой должности нет!");
+	}
+}
+addDrinks(title, num){
+	var water = new Drink(title, num);
+	this.drinks.push(water);
+	return this;
+}
+removeStaff(name){
+	if(this.bar.find(person => person.name === name)){
+		return delete person.name;
+	} else if(this.waiter.find(person => person.name === name)){
+		return delete person.name;
+	} else{ console.log("Такого сотрудника нету!!!!")}
+}
+}
+
+class Person{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+}
+
+class Barman extends Person{
+	constructor(name, age, theBestDrink, place){
+		super(name, age);
+		this.theBestDrink = theBestDrink;
+		this.place = "Barman";
+	}
+	theBestDrink(){
+	 console.log(`Сегодня фирменный коктейль - ${this.topDrink}!`);
+
+}
+doOrder(name, title, quantity){
+ 	for(let i = 0; i<name.order.length; i++){
+ 	if(name.order[i].title === title){
+ 		let drinkQuantity = name.order[i].num -= quantity;
+ 		if(drinkQuantity > 0){
+ 			return name.order[i].num -= quantity;
+ 		} else {
+ 			return alert("У нас столько нет!");
+ 		}
+
+ 	} else {
+ 		return alert("У нас нету такого напитка");
+ 	}
+}
+ }
+ getPlace (){
+ 	return "Barman"
+ }
+}
+
+
+
+class Waiter extends Person{
+	constructor(name, age, place){
+		super(name, age);
+		this.place = "Waiter";
+	}
+	addOrder(name, title, num){
+ 	var order = new Drink(title, num);
+ 	 if(name.drinks.find(product => product.title === title)){
+ 	return name.order.push(order);
+ } else {
+ 	return alert("Такого напитка нет.");
+ 	}
+ }
+}
+
+
+let bar = new Bar("Bar");
+const sam = new Waiter("Sam", 23);
+ const bob = new Waiter("Bob", 21);
+ const elise = new Waiter("Elise", 27);
+ const don = new Waiter("Don", 30);
+
+ const edic = new Barman("Edic", 26, "Russian vodka");
+ const alik = new Barman("Alik", 32, "Bomba");
+
+
+bar
+	.addStaff(sam)
+	.addStaff(bob)
+	.addStaff(elise)
+	.addStaff(don)
+	.addStaff(edic)
+	.addStaff(alik)
+	.addDrinks("vodka", 10)
+	.addDrinks("juce", 5)
+	.addDrinks("Byrbone", 3);
+	bob.addOrder(bar, "vodka", 5);
+	edic.doOrder(bar, "vodka", 6);
+	
+console.log(bar);
+
+
+*/
+
+
+
+
+
+
